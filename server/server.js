2d5b8cc65554760eca5b351829c9bf067dd991b2
mc4rol9 <mariacarol9@gmail.com>
2018-01-30 15:01:18 -0200

    server: initial config

--- server/server.js
+++ server/server.js
@@ -2,7 +2,18 @@
 // TODO APP - ROOT FILE
 //
 
+// load in libraries
+var express = require('express');
+var bodyParser = require('body-parser');
+
 // import/load in project files
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+// setup app
+var app = express();
+
+app.listen(3000, () => {
+    console.log('Started n port 3000');
+});
