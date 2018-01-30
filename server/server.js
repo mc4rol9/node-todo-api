127635a97bb77c8a3dfa3523605d7f0e119d5c09
mc4rol9 <mariacarol9@gmail.com>
2018-01-30 17:13:37 -0200

    feat: add GET /todos by id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@
 // load in libraries
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectId} = require('mongodb');
 
 // import/load in project files
 var {mongoose} = require('./db/mongoose');
@@ -37,6 +38,25 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// get todo by id
+// fetch variable passed from url with :id - dynamic
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
