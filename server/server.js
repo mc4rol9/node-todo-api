64ce6148b382711bf2dddce8a2a438c342febb90
mc4rol9 <mariacarol9@gmail.com>
2018-01-30 15:21:27 -0200

    server: add post route to create todo item

--- server/server.js
+++ server/server.js
@@ -7,12 +7,29 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 // import/load in project files
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var {mongoose} = require('../db/mongoose');
+var {Todo} = require('../models/todo');
+var {User} = require('../models/user');
 
 // setup app
 var app = express();
+
+// middleware
+app.use(bodyParser.json());
+
+// routes/endpoint
+// create todo
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started n port 3000');
