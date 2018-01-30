9aa34ea665403c2c1dc596ec0b72588c98f23ea5
mc4rol9 <mariacarol9@gmail.com>
2018-01-30 13:16:26 -0200

    Create Todo model

--- server/server.js
+++ server/server.js
@@ -9,3 +9,16 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // connect to db
 mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+// create model
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
