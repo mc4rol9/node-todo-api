0b5972a94a4fced49ad9182709d0403589cd2c4f
mc4rol9 <mariacarol9@gmail.com>
2018-01-30 15:41:25 -0200

    test: Create POST test case

--- tests/server.test.js
+++ tests/server.test.js
@@ -9,3 +9,29 @@ const request = require('supertest');
 // load in project files
 const {app} = require('../server');
 const {Todo} = require('../models/todo');
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+            .post('/todos')
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text)toBe(text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                // fetch everything in the collection
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
