0729ae0f11432a4032deae3a083b0ce8f0517b73
mc4rol9 <mariacarol9@gmail.com>
2018-01-30 22:14:38 -0200

    feat: add properties to users model for auth

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@
 //
 
 // load in mongoose
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 // user model
 var User = mongoose.model('User', {
@@ -11,8 +12,28 @@ var User = mongoose.model('User', {
         type: String,
         required: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
     // create new user for testing
